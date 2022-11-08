9b89a2a1b9f30142423f35f2647b59c98831793b
Daegonny <igorchagasm@hotmail.com>
2022-11-08 13:28:16 -0300

    feat: add comment page view

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import PostsView from '../views/PostsView.vue'
+import PostView from '../views/PostView.vue'
+import AuthorView from '../views/AuthorView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'posts',
+      component: PostsView
     },
+    { path: '/post/:id', name: 'post', component: PostView },
     {
-      path: '/about',
-      name: 'about',
+      path: '/authors',
+      name: 'authors',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      component: () => import('../views/AuthorsView.vue')
+    },
+    { path: '/author/:username', name: 'author', component: AuthorView }
   ]
 })
 
